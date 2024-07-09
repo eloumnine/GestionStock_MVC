73da69f38c2d0b75f6b9d0852ba9908d7826919d
abdelhakim.eloumnine <eloumnine@gmail.com>
2024-07-09 19:24:37 +0100

    Initial Commit - Project Gestion STOCK

---
+++ GestionStock_MVC/src/test/java/org/tradiv/afrique/stock/GestionStockMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tradiv.afrique.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
